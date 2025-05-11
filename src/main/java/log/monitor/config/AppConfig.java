eb9c14d1fc6d1321a13b4d8aa5e5fbcc6cd923f5
sebybuga <seby_buga@yahoo.com>
2025-05-11 03:13:22 +0300

    LogMonitor - Unused Bean deleted

--- src/main/java/log/monitor/config/AppConfig.java
+++ src/main/java/log/monitor/config/AppConfig.java
@@ -1,26 +1,13 @@
 package log.monitor.config;
 
-import log.monitor.service.AlertService;
-import log.monitor.service.LogParserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration(proxyBeanMethods = false) // Disable CGLIB proxying
+@Configuration(proxyBeanMethods = false)
 public class AppConfig {
 
     @Bean
     public AlertConfigInterface alertConfig() {
-        // Assuming the properties file is "application.properties" in the classpath
         return new AlertConfig("application.properties");
-    }
-
-    @Bean
-    public AlertService alertService(AlertConfigInterface alertConfig) {
-        return new AlertService(alertConfig);
-    }
-
-    @Bean
-    public LogParserService logParserService(AlertService alertService) {
-        return new LogParserService(alertService);
     }
 }
